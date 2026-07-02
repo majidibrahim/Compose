fa51ef8ced8bcd8a86747880771d7a0830044929
majid <37274496+majidibrahim@users.noreply.github.com>
2026-07-02 10:02:29 +0300

    first commit

--- app/src/main/java/com/example/compose_unlimited/Compose_Foundation/Text/Text.kt
+++ app/src/main/java/com/example/compose_unlimited/Compose_Foundation/Text/Text.kt
@@ -1,1 +1,10 @@
 package com.example.compose_unlimited.Compose_Foundation.Text
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Text(text: String) {
+    Text(
+        text = text
+    )
+}
